132f1fcf7c071312e2f784140bf35e3271bb5c41
fukeasahi <62973488+fukeasahi@users.noreply.github.com>
2022-03-21 14:55:53 +0900

    不要な記述を削除しました

--- rails_new.sh
+++ rails_new.sh
@@ -5,24 +5,3 @@ docker-compose build
 # passを書き換える
 docker-compose run web rails db:create
 docker-compose up -d
-
-
-
-
-
-
-
-
-
-
-# # 追加のコマンド
-# docker-compose down
-
-# # コンテナの一覧を見るため
-# docker-compose ps
-
-# # logを確認するため
-# docker-compose logs
-
-
-# docker-compose exec web /bin/bash
