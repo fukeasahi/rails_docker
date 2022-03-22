9f68bdf9e89ee3fd537c0635cfe0d8cf547e52d0
fukeasahi <62973488+fukeasahi@users.noreply.github.com>
2022-03-22 15:45:34 +0900

    update sed -i

--- rails_new.sh
+++ rails_new.sh
@@ -3,7 +3,7 @@
 docker-compose run web rails new . --force --database=mysql
 # Gemfileが更新されたから
 docker-compose build
-sed -i -e 's/password:/password: password/g' ./src/config/database.yml
-sed -i -e 's/host: localhost/host: db/g'     ./src/config/database.yml
+sed -i -e '17s/password:/password: password/g' ./src/config/database.yml
+sed -i -e '18s/host: localhost/host: db/g'     ./src/config/database.yml
 docker-compose run web rails db:create
 docker-compose up -d
